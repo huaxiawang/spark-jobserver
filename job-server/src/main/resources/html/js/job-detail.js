dd9c5bc0373b89872a90987d0feb2ed7b669ee60
Huaxia Wang <hwang@transcendinsights.com>
2015-06-01 09:46:22 -0700

    Change D3

--- job-server/src/main/resources/html/js/job-detail.js
+++ job-server/src/main/resources/html/js/job-detail.js
@@ -27,8 +27,9 @@ function showJobDetail(jobId) {
           var chart = nv.models.pieChart()
                   .x(function(d) { return d.label })
                   .y(function(d) { return d.value })
-                  .showLabels(true)     //Display pie labels
-                  .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+                  .showLegend(false)
+                  .showLabels(false)     //Display pie labels
+                  .labelThreshold(.01)  //Configure the minimum slice size for labels to show up
                   .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
                   .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
                   .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
@@ -47,8 +48,9 @@ function showJobDetail(jobId) {
           var chart = nv.models.discreteBarChart()
                   .x(function(d) { return d.label })    //Specify the data accessors.
                   .y(function(d) { return d.value })
-                  .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
-                  .tooltips(false)        //Don't show tooltips
+                  .showXAxis(false)
+                  .staggerLabels(false)    //Too many bars and not enough room? Try staggering labels.
+                  .tooltips(true)        //Don't show tooltips
                   .showValues(true)       //...instead, show the bar value right on top of each bar.
               ;
 
