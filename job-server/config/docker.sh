5993637edb258f1625bcaab9f3752e64dfbab620
Huaxia Wang <hwang@transcendinsights.com>
2015-08-27 14:21:58 -0700

    add config file for docker

--- job-server/config/docker.sh
+++ job-server/config/docker.sh
@@ -2,8 +2,15 @@
 # NOTE: only static vars not intended to be changed by users should appear here, because
 #       this file gets sourced in the middle of server_start.sh, so it will override
 #       any env vars set in the docker run command line.
+DEPLOY_HOSTS="localhost"
+
+APP_USER=root
+APP_GROUP=stuff
+
+INSTALL_DIR=/root/spark-jobserver
 PIDFILE=spark-jobserver.pid
-SPARK_HOME=/spark
+SPARK_HOME=/usr/local/spark
 SPARK_CONF_DIR=$SPARK_HOME/conf
+SCALA_VERSION=2.10.4
 # For Docker, always run start script as foreground
 JOBSERVER_FG=1
